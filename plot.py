beff31f78f089c36903a91ec199d8ecb9d1d4812
Murilo Garcia <murilomiglugy@gmail.com>
2025-01-30 13:01:31 -0300

    Plot E e tentando o B
    Nesse está o plot enquanto o campo B estamos tentando calcular e colocá-lo junto ao plot

--- plot.py
+++ plot.py
@@ -17,18 +17,29 @@ for inicio in range(0,len(data_frame), n_vet):
     media = data_frame.loc[inicio:final,"Tempo(s)"].mean()
     data_frame.loc[inicio:final] = media
 print("Média a cada 125 vetores")
-print(data_frame["Tempo(s)"])
+print("Média a cada 125 vetores")
+#----------------------------------------------------------------------
+
+U = data_frame.loc[inicio:final, "Coordenada_U(m)"]
+V = data_frame.loc[inicio:final, "Coordenada_V(m)"]
+W = data_frame.loc[inicio:final, "Coordenada_W(m)"]
+#     #Coordenadas dos vetores
+X = data_frame.loc[inicio:final, "Coordenada_X(m)"]
+Y = data_frame.loc[inicio:final, "Coordenada_Y(m)"]
+Z = data_frame.loc[inicio:final, "Coordenada_Z(m)"]
+
+
+for i in range(125):
+      v = [X.iloc[i],Y.iloc[i],Z.iloc[i]]
+      v = normaliza_vetor(v)
+      velocidade = [x*velocidade[0], y*velocidade[1],z*velocidade[2]]
+      x = v[0]
+      y = v[1]
+      z = v[2]
+      A = velocidade/(c * np.linalg.norm(v))
+      B = curl(A)
+      ax.quiver(U.iloc[i],V.iloc[i],W.iloc[i],v[0],v[1],v[2], pivot = 'middle', color = 'green')
+      #ax.quiver(U.iloc[i],V.iloc[i],W.iloc[i], B[0],B[1],B[2], pivot = 'middle', color = 'red')
+print(B)
+plt.show()
 #-----------------------------------------------------------------------
-
-
-
-#Trazendo a lista de posições de cada vetor respectivo
-t = data_frame["Tempo(s)"] #Lista dos instantes de tempo
-
-subset = data_frame[data_frame["Tempo(s)"] == data_frame.loc[124, "Tempo(s)"]]
-#-----------------------------------------------------------------------
-print(subset)
-#
-
-
-#havia criado o plot aqui mas apaguei mais de uma vez devido a organização e achar os erros
